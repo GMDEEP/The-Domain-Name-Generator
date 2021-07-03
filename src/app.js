d56680246e148c447c31ae90e1a2fffbd60c34dd
GMDEEP <stewartjgreen@gmail.com>
2021-07-03 02:41:29 +0000

    update1

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+    //write your code here
+    let pronoun = ["the", "our"];
+    let adj = ["great", "big"];
+    let noun = ["jogger", "racoon"];
+    let extensions = [".com", ".net", ".us", ".io", ".etc"];
+    let newArray = [];
+for (let index = 0; index < pronoun.length; index++) {
+    const newArray.push() = pronoun[index];
+    console.log(newArray);
+    
+    
+}
+}
+console.log("Hello Rigo from the console!");
 };
