71bd18661a9a4d50da1c593e263b13c9bb1ebc56
GMDEEP <stewartjgreen@gmail.com>
2021-07-03 04:37:26 +0000

    Update 4

--- src/app.js
+++ src/app.js
@@ -5,19 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function () {
-    //write your code here
-    let pronoun = ["the", "our"];
-    let adj = ["great", "big"];
-    let noun = ["jogger", "racoon"];
-    let extensions = [".com", ".net", ".us", ".io", ".etc"];
-    let newArray = [];
-for (let index = 0; index < pronoun.length; index++) {
-    const newArray.push() = pronoun[index];
-    console.log(newArray);
-    
-    
-}
-}
-console.log("Hello Rigo from the console!");
+window.onload = function() {
+  //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extensions = [".com", ".net", ".us", ".io", ".etc"];
+
+  const randomIndex = lengthOfArray => {
+    return Math.floor(Math.random() * lengthOfArray);
+  };
+
+  console.log(pronoun[randomIndex(pronoun.length)]);
+  console.log(adj[randomIndex(adj.length)]);
+  console.log(noun[randomIndex(noun.length)]);
+  console.log(extensions[randomIndex(extensions.length)]);
+  document.getElementById("The Domain Name Generator").innerHTML =
+    pronoun[randomIndex(pronoun.length)] +
+    " " +
+    adj[randomIndex(adj.length)] +
+    " " +
+    noun[randomIndex(noun.length)] +
+    " " +
+    extensions[randomIndex(extensions.length)];
 };
